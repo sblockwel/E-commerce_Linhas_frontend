4277c16ac07e8a465cdb494d15a01d2674155c36
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-11-21 21:19:06 -0300

    Organizando arquivos do projeto

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/guest/Home.vue'
 import store from "../store";
-import Register from "../views/Register.vue";
-import Login from "../views/Login.vue";
+import Register from "../views/auth/Register.vue";
+import Login from "../views/auth/Login.vue";
 
 Vue.use(VueRouter)
 
@@ -17,7 +17,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import('../views/guest/About.vue')
   },
   {
     path: "/register",
