cc3c0841e0798fdc6f1de671610e3b7c823b0e70
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-11-22 21:51:56 -0300

    Adicionando componentes do vuetify
    Ainda não finalizado

--- src/views/auth/Register.vue
+++ src/views/auth/Register.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="center">
     <div>
+      <v-form ref="form" v-model="valid" lazy-validation>
+
+        <v-text-field v-model="form.username" :counter="10" :rules="nameRules" label="Nome de usuário" required ></v-text-field>
+
+        <v-text-field v-model="form. full_name" :rules="nameRules" label="Nome completo" required ></v-text-field>
+
+        <v-text-field v-model="form.email" :rules="emailRules" label="E-mail" required ></v-text-field>
+
+        <v-text-field v-model="form.document" :rules="documentRules" label="CPF ou CNPJ" required ></v-text-field>
+
+        <v-text-field v-model="form.password" :rules="passwordRules" label="Senha" required ></v-text-field>
+
+        <v-text-field v-model="form.confirmPassword" :rules="confirmPasswordRules" label="Confirme a senha" required ></v-text-field>
+        <!--
+          <v-select v-model="select" :items="items" :rules="[(v) => !!v || 'Item is required']" label="Item" required ></v-select>
+        
+        <v-checkbox v-model="checkbox" :rules="[(v) => !!v || 'You must agree to continue!']" label="Do you agree?" required ></v-checkbox>
+        -->
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate" >
+          Validate
+        </v-btn>
+
+        <v-btn color="error" class="mr-4" @click="reset"> Reset Form </v-btn>
+
+        <v-btn color="warning" @click="resetValidation"> Reset Validation </v-btn>
+
+      </v-form>
+
       <form @submit.prevent="submit">
         <div>
           <label for="username">Username:</label>
@@ -17,14 +45,18 @@
         <div>
           <label for="document">document:</label>
           <input type="text" name="document" v-model="form.document" />
-        </div>        
+        </div>
         <div>
           <label for="password">Password:</label>
           <input type="password" name="password" v-model="form.password" />
         </div>
         <div>
           <label for="confirmPassword"> confirmPassword :</label>
-          <input type="password" name="confirmPassword" v-model="form.confirmPassword" />
+          <input
+            type="password"
+            name="confirmPassword"
+            v-model="form.confirmPassword"
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -47,7 +79,7 @@ export default {
         password: "",
         email: "",
         document: "",
-        confirmPassword: ""
+        confirmPassword: "",
       },
       showError: false,
     };
@@ -56,14 +88,14 @@ export default {
     ...mapActions(["Register"]),
     async submit() {
       try {
-          console.log("antes")
+        console.log("antes");
         await this.Register(this.form);
-        console.log("depois")
+        console.log("depois");
         this.$router.push("/login");
         this.showError = false;
       } catch (error) {
         this.showError = true;
-        console.log(error)
+        console.log(error);
       }
     },
   },
