709ccf28a6412ff8f2dbf412bdc25f3b96fcea71
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-05 22:15:10 -0300

    ajustes

--- src/router/clientRoutes.js
+++ src/router/clientRoutes.js
@@ -1,26 +1,26 @@
 const routes = [
-    {
-      path: 'home',
-      name: 'Home',
-      component: () => import('../views/guest/Home.vue'),
-    },
-    {
-        path: 'about',
-        name: 'About',
-        component: () => import('../views/guest/About.vue')
-      },
-      {
-        path: "register",
-        name: "Register",
-        component: () => import('../views/auth/Register.vue'),
-        meta: { guest: true },
-      },
-      {
-        path: "login",
-        name: "Login",
-        component: () => import('../views/auth/Login.vue'),
-        meta: { guest: true },
-      },
-  ]
+  {
+    path: 'home',
+    name: 'Home',
+    component: () => import('../views/guest/Home.vue'),
+  },
+  {
+    path: 'about',
+    name: 'About',
+    component: () => import('../views/guest/About.vue')
+  },
+  {
+    path: "register",
+    name: "Register",
+    component: () => import('../views/auth/Register.vue'),
+    meta: { guest: true },
+  },
+  {
+    path: "login",
+    name: "Login",
+    component: () => import('../views/auth/Login.vue'),
+    meta: { guest: true },
+  },
+]
 
 export default routes
