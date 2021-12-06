000e64bc0a0d41e61079a00860c25bd48575b736
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-05 22:19:25 -0300

    adicionando rotas das novas telas

--- src/router/adminRoutes.js
+++ src/router/adminRoutes.js
@@ -4,6 +4,36 @@ const routes = [
       name: 'HomeAdmin',
       component: () => import('../views/admin/Home.vue'),
     },
+    {
+      path: '/product',
+      name: 'ProductCreate',
+      component: () => import('../views/admin/ProductForm.vue'),
+    },
+    {
+      path: '/product/:id',
+      name: 'ProductEdit',
+      component: () => import('../views/admin/ProductForm.vue'),
+    },
+    {
+      path: '/products',
+      name: 'ProductList',
+      component: () => import('../views/admin/ProductList.vue'),
+    },
+    {
+      path: '/category',
+      name: 'CategoryCreate',
+      component: () => import('../views/admin/CategoryForm.vue'),
+    },
+    {
+      path: '/category/:id',
+      name: 'CategoryEdit',
+      component: () => import('../views/admin/ProductForm.vue'),
+    },
+    {
+      path: '/categories',
+      name: 'CategoryList',
+      component: () => import('../views/admin/CategoryList.vue'),
+    },
   ]
 
 export default routes
