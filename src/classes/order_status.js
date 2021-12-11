b9c7618b0319909a8a648bc8816908767da591ea
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-10 22:58:57 -0300

    Adicionando e ajustado Lista de pedidos pendentes

---
+++ src/classes/order_status.js
@@ -0,0 +1,13 @@
+const OrderPending=0
+const OrderWaiting=1
+const OrderProcessing=2
+const OrderFinished=3
+const OrderCancelled=4
+
+export {
+    OrderCancelled,
+    OrderFinished,
+    OrderProcessing,
+    OrderWaiting,
+    OrderPending
+}
--- src/views/admin/OrderList.vue
+++ src/views/admin/OrderList.vue
@@ -1,46 +1,107 @@
 <template>
   <div>
-      
-
     <v-data-table
       :headers="headers"
-      :items="categories"
+      :items="orders"
       :items-per-page="5"
-      class="elevation-1"
-    ></v-data-table>
+      class="elevation-1">
+      <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="finalizeOrder(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="cancelOrder(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import { OrderFinished, OrderCancelled } from "../../classes/order_status.js"
+
 export default {
   name: "OrderList",
   data() {
     return {
       headers: [
         {
-          text: "Nome",
+          text: "Número do pedido",
           align: "start",
-          value: "name",
+          value: "orderNumber",
         },
+        {
+          text: "Data de emissão",
+          align: "start",
+          value: "creationDate",
+        },
+        {
+          text: "Cliente",
+          align: "start",
+          value: "client.name",
+        },
+        {
+          text: "Status",
+          align: "start",
+          value: "status",
+        },
+        { text: 'Ações', value: 'actions', sortable: false }
       ],
-      categories: [
+      orders: [
         {
-          name: "Frozen Yogurt",
+          orderNumber: "Frozen Yogurt",
+          creationDate: 15.25,
+          client: {
+            name: "sabrina"
+          },
+          status: "Pendente",
         },
         {
-          name: "Ice cream sandwich",
+          orderNumber: "Frozen Yogurt",
+          creationDate: 15.25,
+          client: {
+            name: "sabrina"
+          },
+          status: "Pendente",
         },
         {
-          name: "Eclair",
+          orderNumber: "Frozen Yogurt",
+          creationDate: 15.25,
+          client: {
+            name: "sabrina"
+          },
+          status: "Pendente",
         },
       ],
     };
   },
   methods:{
-
+    ...mapActions(["SetOrderStatus"]),
+    async finalizeOrder(order){
+        await this.SetOrderStatus(order.id, OrderFinished)
+      },
+      async cancelOrder(order){
+        await this.SetOrderStatus(order.id, OrderCancelled)
+      }
   },
   async mounted() {
-      this.orders = await this.GetOrders();
+     // this.orders = await this.GetOrders();
   },
 };
 </script>
