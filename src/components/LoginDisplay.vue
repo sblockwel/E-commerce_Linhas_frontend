301a54f9d0ef90e1dc85aa2ea44d536de2c2a1ee
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-12 17:42:21 -0300

    Implementado menu do perfil

--- src/components/LoginDisplay.vue
+++ src/components/LoginDisplay.vue
@@ -1,34 +1,96 @@
 <template>
-    <div v-if="isAuthenticated">
-        <v-btn @click="sair()" text>
-          <span class="mr-2">sair </span>
-          <v-icon>fas fa-sign-out-alt</v-icon>
+  <div v-if="isAuthenticated">
+    <v-menu bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn dark icon v-bind="attrs" v-on="on">
+          <v-avatar size="36px">
+            <img alt="Avatar" :src="imgProfile" />
+          </v-avatar>
         </v-btn>
-      </div>
-      <div v-else>
-        <v-btn href="/login" text> Login </v-btn>
-        <v-btn href="/register" text> Registrar-se </v-btn>
-    </div>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in menuItems"
+          :key="i"
+          link
+          @click="item.action"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+  <div v-else>
+    <v-btn href="/login" text> Login </v-btn>
+    <v-btn href="/register" text> Registrar-se </v-btn>
+  </div>
 </template>
 
 <script>
-
 import { mapActions, mapGetters } from "vuex";
+import routes from "../router/adminRoutes.js";
 export default {
-    name: "LoginDisplay",
-    methods: {
-        ...mapActions(["LogOut"]),
-        async sair() {
-        await this.LogOut();
-        this.$router.push("/login");
+  name: "LoginDisplay",
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "Sair",
+          icon: "fas fa-sign-out-alt",
+          action: this.sair,
         },
+      ],
+      orders: [],
+    };
+  },
+  methods: {
+    ...mapActions(["LogOut"]),
+    async sair() {
+      await this.LogOut();
+      this.$router.push("/login");
     },
-    computed: {
-        ...mapGetters({ isAuthenticated: "isAuthenticated" }),
+    goToPanel() {
+      if (this.$router.currentRoute.path != "/admin") {
+        this.$router.push({ name: "HomeAdmin" });
+      }
     },
+    goToStore() {
+      if (this.$router.currentRoute.path != "/home") {
+        this.$router.push({ name: "Home" });
+      }
+    },
+    isPageAdmin() {
+      let pageAdmin = false;
+      for (let route in routes) {
+        if (this.$router.currentRoute.name == routes[route].name) {
+          pageAdmin = true;
+        }
+      }
+      return pageAdmin;
     }
+  },
+  computed: {
+    ...mapGetters({ isAuthenticated: "isAuthenticated", isAdmin: "isAdmin", imgProfile: "ProfileImg" }),
+  },
+  mounted() {
+      console.log(this.imgProfile)
+    if (this.isAdmin && !this.isPageAdmin()) {
+      this.menuItems.splice(0, 0, {
+        title: "Painel gerencial",
+        icon: "fas fa-sign-out-alt",
+        action: this.goToPanel,
+      });
+    } else if (this.isPageAdmin()) {
+      this.menuItems.splice(0, 0, {
+        title: "Ir para a loja",
+        icon: "fas fa-basket",
+        action: this.goToStore,
+      });
+    }
+  },
+};
 </script>
 
 <style>
-
 </style>
