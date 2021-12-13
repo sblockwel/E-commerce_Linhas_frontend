3980dc4dd7cf86b65bafb430f6d0797b221cf46d
sblockwel <63020705+sblockwel@users.noreply.github.com>
2021-12-13 18:45:14 -0300

    Update Compra.vue

--- src/views/guest/Compra.vue
+++ src/views/guest/Compra.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="compra">
-    <h1> Descrição compra</h1>
-
+    <h1 id="teste"> Compra Aki </h1>
+  <div>
+    <v-data-table
+      :headers="headers"
+      :items="products"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
      <v-card-actions>
           <v-btn @click="goToVoltar()" text color="orange">Voltar</v-btn>
         </v-card-actions>
@@ -9,12 +16,78 @@
 </template>
 
 <script>
-  export default {
-    
-     methods: {
-    goToVoltar(){
+import { mapActions } from "vuex";
+export default {
+  name: "ProductList",
+  data() {
+    return {
+      headers: [
+        { text: "Nome", align: "start", value: "name" },
+        { text: "Preço", value: "price" },
+        { text: "Quantidade em estoque", value: "quantity" },
+        { text: "Descrição", value: "description" },
+        { text: "Categoria", value: "category.name" },
+      ],
+      products: [
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+        {
+          name: "Frozen Yogurt",
+          price: 15.25,
+          quantity: 6,
+          description: "hbhabfhjbhdbchsbh",
+          category: 1,
+        },
+      ],
+    };
+  },
+  methods: {
+      ...mapActions(["GetProducts"]),
+  },
+      goToVoltar(){
       this.$router.push({name: "Home"})
-    }
-  }
-  }
+    },
+  async mounted() {
+    this.products = await this.GetProducts();
+  },
+};
 </script>
+
+<style>
+teste{
+text-align: center;
+}
+</style>
