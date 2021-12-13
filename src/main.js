1ea63015f14ffc439ce2f18d63f494555502fdc2
Sabrina Blockwel <sblockwel@gmail.com>
2021-12-13 11:24:53 -0300

    Alterado URL do backend para usar o heroku

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://e-commerce-linhas.herokuapp.com";
 
 axios.interceptors.response.use(undefined, function(error) {
   if (error) {
