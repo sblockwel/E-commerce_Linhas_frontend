16c821e6923955d221ee8737bbb479b331b6a494
Sabrina Blockwel <sblockwel@gmail.com>
2021-12-13 12:31:32 -0300

    Adicionado arquivos compilados

---
+++ dist/js/chunk-2d0c49ae.439cb0d8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c49ae"],{"3c19":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v(" Oi eu sou a home admin")])])}],a={name:"Home"},r=a,i=t("2877"),l=Object(i["a"])(r,c,u,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0c49ae.439cb0d8.js.map
